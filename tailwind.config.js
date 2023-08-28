02eb8e258bf0b6d86030345fbf536e038c4f02b6
Furquim~ <lucasviniu897@gmail.com>
2023-08-28 20:08:07 -0300

    arumando arqui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [],
+  darkMode: false,
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: "#c542f5",
+          200: "#9e35c4",
+          300: "#6d2487",
+        },
+      },
+    },
   },
   plugins: [require("prettier-plugin-tailwindcss")],
 };
